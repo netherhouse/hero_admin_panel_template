f6284b1f8c08bf239362563613685384bc49ad5c
netherhouse <nether3ord@gmail.com>
2024-12-23 02:50:06 +0100

    сделал красиво код

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./styles/index.scss";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
