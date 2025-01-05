239aa403393d9cc398ef474db3e29cf79b834ac2
netherhouse <nether3ord@gmail.com>
2025-01-05 15:18:08 +0100

    store enchancer

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,28 @@ import { createStore, combineReducers } from "redux";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
+const enhancer =
+  (createStore) =>
+  (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+      if (typeof action === "string") {
+        return oldDispatch({
+          type: action,
+        });
+      }
+      return oldDispatch(action);
+    };
+    return store;
+  };
+
 const store = createStore(
   combineReducers({ heroes: heroes, filters: filters }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  enhancer
 );
 
 export default store;
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
