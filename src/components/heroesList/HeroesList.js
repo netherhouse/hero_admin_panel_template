781bfc43743071be0162f1b09df3e4c3b48a7381
netherhouse <nether3ord@gmail.com>
2025-01-09 13:21:30 +0100

    createAsyncThunk + добраотки и улучешие в hook & slice and list

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -3,13 +3,12 @@ import { useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import { fetchHeroes } from "../../actions";
-import { heroDeleted } from "./heroesSlice";
+import { heroDeleted, fetchHeroes } from "./heroesSlice";
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from "../spinner/Spinner";
 
 import "./heroesList.scss";
-import { createSelector } from "reselect";
+import { createSelector } from "@reduxjs/toolkit";
 
 const HeroesList = () => {
   const filteredHeroesSelector = createSelector(
